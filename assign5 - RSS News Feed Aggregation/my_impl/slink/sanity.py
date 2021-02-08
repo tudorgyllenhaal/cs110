8d83b8eaa79ddb042914f4454f0306e8fc9265d1
Sean Lee <li.yaonan@wustl.edu>
2021-02-08 03:45:40 -0800

    username wrong

--- assign5 - RSS News Feed Aggregation/my_impl/slink/sanity.py
+++
@@ -1,6 +0,0 @@
-
-def eliminate_news_logging(str):
-   _, prompt, results = str.partition("Enter a search term [or just hit <enter> to quit]:")
-   if prompt == '': # If the prompt isn't found, just return the unfiltered stuff
-      return str
-   return results
